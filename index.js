1cf5620ea8c27f6143b51f5d122c968e8b7f39c0
RichardOkongo <rirachard19@gmail.com>
2023-01-06 18:19:19 +0300

    added own test

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+    return true
+  } else {
+    return false
+  }
+  }
+
 
 /* 
   Add your pseudocode here
+   type in input string
+  reverse the input string
+   if the input string is the same as the reverse
+     return true
+   else
+   return false
 */
 
 /*
   Add written explanation of your solution here
-*/
+A string is considere a plindrome if the it is read forward and read backwards it returns the same result for example nursesrun
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  onsole.log("Expecting: true");
+  console.log("=>", isPalindrome("nursesrun"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
